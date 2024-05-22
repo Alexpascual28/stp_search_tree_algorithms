9db9c083e1b59891fe0977470245d227bd670d5a
Alexpascual28 <apsr2809@gmail.com>
2024-05-22 19:31:13 +0100

    Updated for Python 3.9

--- 201255350/qc_tester.py
+++ 201255350/qc_tester.py
@@ -11,17 +11,17 @@ sys.stdout = open('tester_outcome.txt', 'w')
 def zero_heuristic(state):
     return 0
 
-search(make_qc_problem(1,1), ('A_star', zero_heuristic), 5000, [])
-search(make_qc_problem(3,3), ('A_star', zero_heuristic), 5000, [])
-search(make_qc_problem(4,4), ('A_star', zero_heuristic), 5000, [])
-search(make_qc_problem(5,5), ('A_star', zero_heuristic), 5000, [])
+search(make_qc_problem(1,1), ('A_star', empty_squares_heuristic), 5000, [])
+search(make_qc_problem(3,3), ('A_star', empty_squares_heuristic), 5000, [])
+# search(make_qc_problem(4,4), ('A_star', empty_squares_heuristic), 5000, [])
+# search(make_qc_problem(5,5), ('A_star', empty_squares_heuristic), 5000, [])
 
-search(make_qc_problem(5,6), ('A_star', zero_heuristic), 5000, [])
-search(make_qc_problem(6,5), ('A_star', zero_heuristic), 5000, [])
-search(make_qc_problem(10,3), ('A_star', zero_heuristic), 5000, [])
+# search(make_qc_problem(5,6), ('A_star', empty_squares_heuristic), 5000, [])
+# search(make_qc_problem(6,5), ('A_star', empty_squares_heuristic), 5000, [])
+# search(make_qc_problem(10,3), ('A_star', empty_squares_heuristic), 5000, [])
 
-search(make_qc_problem(3,4), ('A_star', zero_heuristic), 5000, [])
-search(make_qc_problem(4,7), ('A_star', zero_heuristic), 5000, [])
-search(make_qc_problem(2,50), ('A_star', zero_heuristic), 5000, [])
+# search(make_qc_problem(3,4), ('A_star', empty_squares_heuristic), 5000, [])
+# search(make_qc_problem(4,7), ('A_star', empty_squares_heuristic), 5000, [])
+# search(make_qc_problem(2,50), ('A_star', empty_squares_heuristic), 5000, [])
 
 

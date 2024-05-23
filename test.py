033f37833db2757461c42c7338bae4e4041ef1f6
Alexpascual28 <apsr2809@gmail.com>
2024-05-23 10:33:14 +0100

    surrounding squares test

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 def get_surrounding_squares(position):
     x, y = position
 
-    for x, y in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
+    for x, y in [(x-1, y-1), (x, y-1), (x+1, y-1), (x-1, y), (x+1, y), (x-1, y+1), (x, y+1), (x+1, y+1)]:
         if x >= 0 and y >= 0:
             yield x, y
 
-surrounding_squares = get_surrounding_squares((0, 0))
+surrounding_squares = get_surrounding_squares((3, 5))
 
 print(list(surrounding_squares))

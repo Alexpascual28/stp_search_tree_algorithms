e7af4e91ec5383dd3ac58dde5bc85bacc62239c4
Alexpascual28 <apsr2809@gmail.com>
2024-06-20 16:14:29 +0100

    feat: Add margins to blind informed search

--- other_problems/maze_problem/blind_informed_search.py
+++ other_problems/maze_problem/blind_informed_search.py
@@ -11,7 +11,7 @@ from solver.queue_search  import *
 from maze_problem import *
 from sample_mazes import *
 
-def blind_informed_search(actual_maze, initial_known_maze, strategy=('A_star', distance_to_end_heuristic, avoid_turns_cost_function), scan_diagonals=False, print_goal_states=False):
+def blind_informed_search(actual_maze, initial_known_maze, strategy=('A_star', distance_to_end_heuristic, avoid_turns_cost_function), scan_diagonals=False, print_goal_states=False, margins=0):
     # Check if the actual and initial mazes are the same size
     if are_mazes_equal_size(actual_maze, initial_known_maze) == False:
         raise Exception("Actual and initial mazes are different sizes. The initial unknown maze should be the same size as the actual maze.")
@@ -22,8 +22,11 @@ def blind_informed_search(actual_maze, initial_known_maze, strategy=('A_star', d
     print("\nThe initial known maze is:")
     print_maze(initial_known_maze)
 
-    # Current known maze
-    known_maze = deepcopy(initial_known_maze)
+    # Add margins to the actual maze
+    actual_maze_with_margins = add_obstacle_margins(deepcopy(actual_maze), margins)
+
+    # Current known maze (with added margins)
+    known_maze = add_obstacle_margins(deepcopy(initial_known_maze), margins)
 
     # Draw the start and end points in the "final" known maze
     final_known_maze = deepcopy(actual_maze)
@@ -40,7 +43,7 @@ def blind_informed_search(actual_maze, initial_known_maze, strategy=('A_star', d
         path = search(create_maze_problem(known_maze, scan_diagonals, print_goal_states), strategy, 500000, [])
 
         # Follow the path to the goal and re-assign the known maze
-        is_goal_reached, visited_path, known_maze = follow_path(actual_maze, known_maze, path)
+        is_goal_reached, visited_path, known_maze = follow_path(actual_maze_with_margins, known_maze, path)
 
         # Draw path in the final known maze
         final_known_maze, move_number = draw_path(final_known_maze, visited_path, move_number)
@@ -110,5 +113,22 @@ def print_maze(maze):
                 print("%4i" % square, end='')
         print()
 
-blind_informed_search(yora_maze_1, yora_maze_empty, strategy=('A_star', distance_to_end_heuristic, avoid_turns_cost_function) ,print_goal_states=True)
+def add_obstacle_margins(maze, margin):
+    for i in range(len(maze)):
+        for j in range(len(maze[i])):
+            if maze[i][j] == -2:
+                for k in range(-margin, margin+1):
+                    for l in range(-margin, margin+1):
+                        if i+k >= 0 and i+k < len(maze) and j+l >= 0 and j+l < len(maze[i]):
+                            if maze[i+k][j+l] == 0:
+                                maze[i+k][j+l] = -4
+    for i in range(len(maze)):
+        for j in range(len(maze[i])):
+            if maze[i][j] == -4:
+                maze[i][j] = -2
+    return maze
+
+# print_maze(add_obstacle_margins(yora_maze_1, 1))
+
+blind_informed_search(yora_maze_1, yora_maze_empty, strategy=('A_star', distance_to_end_heuristic, avoid_turns_cost_function), print_goal_states=True, margins=2)
 # blind_informed_search(maze_initial_state_2, maze_initial_state_empty)
